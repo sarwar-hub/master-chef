467baaaa1aa91d359b8a38b2003db0ae0e53b52a
Sarwar <2plus7sarwar@gmail.com>
2023-05-02 19:48:18 +0600

    added blog and about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h1>about</h1>
+        <div className='mx-2 md:mx-[10%] my-[100px]'>
+            <h1 className='text-5xl md:text-7xl'>About Us</h1>
+            <hr className='my-7' />
         </div>
     );
 };
--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -3,8 +3,25 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            <h1>blogs</h1>
+        <div className='mx-2 md:mx-[10%] my-[100px]'>
+            <h1 className='text-5xl md:text-7xl'>Blogs</h1>
+            <hr className='my-7' />
+            <div className='mb-5'>
+                <h3 className='text-3xl'># </h3>
+                <p className='text-3xl pl-5'>-  </p>
+            </div>
+            <div className='mb-5'>
+                <h3 className='text-3xl'># </h3>
+                <p className='text-3xl pl-5'>-  </p>
+            </div>
+            <div className='mb-5'>
+                <h3 className='text-3xl'># </h3>
+                <p className='text-3xl pl-5'>-  </p>
+            </div>
+            <div className='mb-5'>
+                <h3 className='text-3xl'># </h3>
+                <p className='text-3xl pl-5'>-  </p>
+            </div>
         </div>
     );
 };
