70b793f5104aeb20d95aaba43843e863cccc9159
Sarwar <2plus7sarwar@gmail.com>
2023-05-02 21:36:45 +0600

    setup firebase

--- src/contexts/AuthProvider/AuthProvider.jsx
+++ src/contexts/AuthProvider/AuthProvider.jsx
@@ -1,16 +1,26 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import app from '../../../../firebase-auth-with-context/src/firebase/firebase.config';
 
+const auth = getAuth(app);
 export const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
-    const user = 'Sarwar';
-    const info = {
-        user
+    const [user, setUser] = useState({});
+
+    // create user
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const data = {
+        user,
+        createUser
     }
     return (
-        <AuthContext.Provider value={info}>
+        <AuthContext.Provider value={data}>
             {children}
         </AuthContext.Provider>
     );
---
+++ src/utilities/firebase-config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAZSSEkkHu1hYY4VN7cZt7jXVDzFH28kEY",
+  authDomain: "master-chef-f1d66.firebaseapp.com",
+  projectId: "master-chef-f1d66",
+  storageBucket: "master-chef-f1d66.appspot.com",
+  messagingSenderId: "508753686263",
+  appId: "1:508753686263:web:0860494feeb28255466df2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
