8415a4b9da96078a4e1fb0eacde6dddb829d6e6f
Sarwar <2plus7sarwar@gmail.com>
2023-05-04 00:01:15 +0600

    added answers in blog page

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -24,21 +24,21 @@ const Blog = () => {
             {/* body */}
             <div ref={ref}>
                 <hr className='my-7' />
-                <div className='mb-5'>
-                    <h3 className='text-3xl'># Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos et officia natus, illum suscipit libero inventore ratione quisquam, cupiditate dolores harum. Molestias fuga consectetur dolorem repudiandae nulla perspiciatis, blanditiis magnam?</h3>
-                    <p className='text-3xl pl-5'>- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde fugiat quos in ratione cumque ipsam consequatur, natus eum suscipit nam voluptatem assumenda odit quam magnam. Nulla ipsum maxime rerum sed. </p>
+                <div className='mb-12'>
+                    <h3 className='text-3xl mb-4'># Differences between uncontrolled and controlled components.</h3>
+                    <p className='text-xl pl-5'>- Controlled components are able to manage or regulate actively and uncontrolled components are not able to manage or regulate actively.</p>
                 </div>
-                <div className='mb-5'>
-                    <h3 className='text-3xl'># Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde fugiat quos in ratione cumque ipsam consequatur, natus eum suscipit nam voluptatem assumenda odit quam magnam. Nulla ipsum maxime rerum sed.</h3>
-                    <p className='text-3xl pl-5'>- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde fugiat quos in ratione cumque ipsam consequatur, natus eum suscipit nam voluptatem assumenda odit quam magnam. Nulla ipsum maxime rerum sed. </p>
+                <div className='mb-12'>
+                    <h3 className='text-3xl mb-3'># How to validate React props using PropTypes?</h3>
+                    <p className='text-xl pl-5'>- Firstly, we have to install the package and import PropTypes. Then have to define propType object and specify the data type with the comoponent. Finally, if the prop is not passed or incorrectly passed then PropTypes will throw an error.</p>
                 </div>
-                <div className='mb-5'>
-                    <h3 className='text-3xl'># Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde fugiat quos in ratione cumque ipsam consequatur, natus eum suscipit nam voluptatem assumenda odit quam magnam. Nulla ipsum maxime rerum sed.</h3>
-                    <p className='text-3xl pl-5'>- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde fugiat quos in ratione cumque ipsam consequatur, natus eum suscipit nam voluptatem assumenda odit quam magnam. Nulla ipsum maxime rerum sed. </p>
+                <div className='mb-12'>
+                    <h3 className='text-3xl mb-3'># Difference between node js and express js.</h3>
+                    <p className='text-xl pl-5'>- Node.js provides the runtime environment for executing JavaScript code on the server, while Express.js is a framework that runs on Node.js and provides additional functionality for building web applications.</p>
                 </div>
-                <div className='mb-5'>
-                    <h3 className='text-3xl'># Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde fugiat quos in ratione cumque ipsam consequatur, natus eum suscipit nam voluptatem assumenda odit quam magnam. Nulla ipsum maxime rerum sed.</h3>
-                    <p className='text-3xl pl-5'>- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde fugiat quos in ratione cumque ipsam consequatur, natus eum suscipit nam voluptatem assumenda odit quam magnam. Nulla ipsum maxime rerum sed. </p>
+                <div className='mb-12'>
+                    <h3 className='text-3xl mb-3'># What is a custom hook, and why will you create a custom hook?</h3>
+                    <p className='text-xl pl-5'>- Custom hooks are custom functions that allows us to extract logic from components and making it easier to reuse code. I will create custom hook to reuse and organise code, manage states etc.</p>
                 </div>
             </div>
         </div>
